fa94c314d18dc6ce8af215a64ed3712fb50cff31
EdersonDeMelo <drsnmelo@gmail.com>
2022-03-29 10:16:43 -0300

    Implementado solucao para mostrar sequencia de primos que geram o numero primo maior do desafio 3

--- src/Desafio02.java
+++ src/Desafio02.java
@@ -1,18 +1,27 @@
 public class Desafio02 {
 
-    public void divisores() {
-        int y = 6;
-        for (int i = y; i > 0; i--) {
-            if (y % i == 0) {
-                System.out.println(i);
+    public void divisores(int num) {
+        for (int i = 1; i <= num; i++) {
+            System.out.print(i + ">>>>>>>");
+            for (int j = i; j > 0; j--) {
+                if (i % j == 0) {
+                    System.out.print(j + ", ");
+                }
             }
+            System.out.println();
+        }
+    }
+    public void quantidadeDeDivisores(){
+        int numeros = 15;
+        for (int i = 0; i < numeros; i++) {
+
         }
     }
 }
 
-class MainDesafio02{
+class MainDesafio02 {
     public static void main(String[] args) {
-    Desafio02 desafio02 = new Desafio02();
-    desafio02.divisores();
+        Desafio02 desafio02 = new Desafio02();
+        desafio02.divisores(1);
     }
 }
--- src/Desafio03.java
+++ src/Desafio03.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 public class Desafio03 {
 
-    public List<Integer> numerosPrimos() {
+    private List<Integer> numerosPrimos() {
         List<Integer> listaDePrimos = new ArrayList<>();
-        for (int i = 2; i <= 50; i++) {
+        for (int i = 2; i <= 500; i++) {
             boolean primo = true;
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
@@ -25,6 +25,7 @@ public class Desafio03 {
         int soma = 0;
         int novaSoma = 0;
         int quantidadeDeNumeros = 0;
+        int sequenciaDePrimos= 0;
 
         for (int i = 0; i < lista.size(); i++) {
             soma += lista.get(i);
@@ -35,7 +36,18 @@ public class Desafio03 {
                     break;
                 }
             }
+
         }
+        for (int j = 0; j < lista.size(); j++) {
+            if (sequenciaDePrimos !=novaSoma){
+                System.out.print(lista.get(j)+", ");
+                sequenciaDePrimos+= lista.get(j);
+                quantidadeDeNumeros++;
+
+            }
+
+        }
+
         System.out.println("Soma " + novaSoma + " quantidade de  numeros primos " + quantidadeDeNumeros);
     }
 }
